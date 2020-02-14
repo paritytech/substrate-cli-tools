8cb4024de89d90c26cd65afff8c43f2499d1a7d8
Kirill Taran <kirill.t256@gmail.com>
2020-02-14 18:54:41 +0100

    Incrementer example ported from Waterfall

---
+++ typescript/test/waterfall/asmscript/incrementer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+root=$(git rev-parse --show-toplevel)
+data=$root/data
+
+cd $root/typescript
+
+rpc_contracts="yarn run ts-node src/contracts.ts"
+
+#$rpc_contracts deploy -f $data/waterfall/asmscript/incrementer.wasm -g 10000
+#$rpc_contracts instantiate -h 0x9a0671b9deaca4a70093fb4785e594229f012ed761c0074389a6b18da4e10d92 -e 1234567DEV -d 0x00 -g 1000000
+$rpc_contracts call -a 5F68HE93fhyagn1DJw9zxqKLZZrdiwjSTyvZPs7c3WHwMavX -e 123DEV -d 0x002a000000 -g 1000000
+$rpc_contracts info -a 5F68HE93fhyagn1DJw9zxqKLZZrdiwjSTyvZPs7c3WHwMavX
