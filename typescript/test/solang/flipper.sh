2b836ada7a58663055a4059766d619149c858217
Sean Young <sean@mess.org>
2020-02-17 15:17:03 +0000

    Make solang flipper test work by recompiling with latest solang
    Signed-off-by: Sean Young <sean@mess.org>

--- typescript/test/solang/flipper.sh
+++ typescript/test/solang/flipper.sh
@@ -9,6 +9,8 @@ cd $root/typescript
 rpc_contracts="yarn run ts-node src/contracts.ts"
 
 $rpc_contracts -f $data/solang/flipper.wasm -g 12345 deploy
-$rpc_contracts -h 0x0854f0d021cc0e5c7297e48b4360dca72edc8d99402f5500877d54b875932633 -e 1234567DEV instantiate -g 12345 -d 0xd531178600
-$rpc_contracts call -a 5DV8tpyoGTtRsr9YFpFK2pzgN4AD1JjTv3SE1jYepuZ9Mdj7 -e 123DEV -d 0xa9efe4cd -g 1234567
-$rpc_contracts info -a 5DV8tpyoGTtRsr9YFpFK2pzgN4AD1JjTv3SE1jYepuZ9Mdj7
+$rpc_contracts -h 0xe553a5690f471b9c110b8044a4db54510d60a30d5cc7925bf7582d177feeb748 -e 1234567DEV instantiate -g 12345 -d 0xd531178600
+$rpc_contracts call -a 5DNB7xw6ko3jH1HubTj5Ch5no2Wn5pt2GwFYsZNr32xiVapL -e 123DEV -d 0xa9efe4cd -g 1234567
+$rpc_contracts info -a 5DNB7xw6ko3jH1HubTj5Ch5no2Wn5pt2GwFYsZNr32xiVapL
+$rpc_contracts call -a 5DNB7xw6ko3jH1HubTj5Ch5no2Wn5pt2GwFYsZNr32xiVapL -e 123DEV -d 0xa9efe4cd -g 1234567
+$rpc_contracts info -a 5DNB7xw6ko3jH1HubTj5Ch5no2Wn5pt2GwFYsZNr32xiVapL
