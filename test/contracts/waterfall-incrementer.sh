f523925ff32aff15a93ab1098bc7d60234d7d56c
Kirill Taran <kirill.t256@gmail.com>
2020-03-27 15:28:16 +0000

    Quiet mode in tests

--- test/contracts/waterfall-incrementer.sh
+++ test/contracts/waterfall-incrementer.sh
@@ -6,7 +6,7 @@ data=$root/data
 
 cd $root/typescript
 
-rpc_contracts="$1contracts$2"
+rpc_contracts="$1contracts$2 -q"
 
 $rpc_contracts deploy -f $data/waterfall/asmscript/incrementer.wasm -g 10000
 $rpc_contracts instantiate -h 0x9a0671b9deaca4a70093fb4785e594229f012ed761c0074389a6b18da4e10d92 -e 1234567DEV -d 0x00 -g 1000000
